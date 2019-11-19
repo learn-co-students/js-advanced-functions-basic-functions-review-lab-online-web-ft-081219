08395e33696bf608cbdef3a15f72797ee6b27a37
Armel <armelfli50@gmail.com>
2019-11-19 14:16:04 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+function saturdayFun(arg="roller-skate"){
+    return `This Saturday, I want to ${arg}!`
+}
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(wraper = "*"){
+    return (arg = "special" )=>{
+        return `You are ${wraper}${arg}${wraper}!`
+    }
+}
+
+const Calculator= {
+    add: (num1 , num2) => {return num1 + num2},
+    subtract: (num1,num2) => {return num1 - num2},
+    multiply: (num1,num2) => {return num1 * num2},
+    divide: (num1,num2) => {return num1 / num2}
+    
+}
+
+function actionApplyer(int, array){
+    if(array.length === 0){
+        return int
+    }else{
+        
+         let result = array.map(e => { return e(int)})
+        return result.push(int)
+    }
+}
