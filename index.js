885d0b137e8196e3a8e5dedf5f7693d3d878bb7b
KHong <albtrossfacefall@gmail.com>
2019-12-04 13:03:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+function saturdayFun(activity='roller-skate') {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity='go to the office') {
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair="*") {
+  return function(param='special') {
+    return `You are ${flair}${param}${flair}!`;
+  }
+}
+
+class DoesMath {
+  constructor() {}
+  
+  add(num1, num2) {return num1 + num2;}
+  
+  subtract(num1, num2) {return num1 - num2;}
+  
+  multiply(num1, num2) {return num1 * num2;}
+  
+  divide(num1, num2) {return num1 / num2;}
+}
+
+let Calculator = new DoesMath;
+
+function actionApplyer(startingInt, array) {
+  if (array.length === 0) {
+    return startingInt
+  } else {
+    return array.reduce((function(newInt,fn) {
+      return fn(newInt);
+    }),startingInt);
+    // return array.reduce(((newInt, fn) => fn(newInt)),startingInt)
+  }
+}
